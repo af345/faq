18b6fb6aefb9002f9927cf42e417e5016c471866
alexandraflores <af345@njit.edu>
2018-11-17 01:16:43 -0500

    Added Answer migrate, model and made answer seeder and added tests for user profile (cleanup), and answers test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
         $this->call(QuestionTableSeeder::class);
+        $this->call(AnswersTableSeeder::class);
     }
 }
